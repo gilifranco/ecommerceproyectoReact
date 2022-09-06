79711ef3af4878f548c36f85839853359c92dcd6
gilifranco <gilifranco1@gmail.com>
2022-09-05 22:02:39 -0300

    proyectocoder

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -5,13 +5,14 @@ import ItemDetailContainer from './ItemDetailContainer';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import NavBoost from './NavBoost';
 const App = () => {
+    const saludo = 'Bienvenidos!'
     return (
         <BrowserRouter>
       <NavBoost/>
         <Routes>
-          <Route path='/' element={ <ItemListContainer />}/>
-          <Route path='/categoria/:categoriaId' element={ <ItemListContainer />}/>
-          <Route path='/producto/:id' element={<ItemDetailContainer/>}/>
+          <Route path='/' element={ <ItemListContainer saludo={saludo} />}/>
+          <Route path='/categoria/:categoriaId' element={ <ItemListContainer saludo={saludo} />}/>
+          <Route path='/detalle/:id' element={<ItemDetailContainer/>}/>
         </Routes>
     </BrowserRouter>
   
